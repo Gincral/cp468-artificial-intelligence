7ab9af8cde87eb3e56c90dd90e6afb6a2624dbae
benkeun <bknarfk@gmail.com>
2020-10-07 12:24:27 -0400

    Added random Puzzle generation for NxN puzzles

--- asgn01/8-puzzle.py
+++ asgn01/8-puzzle.py
@@ -1,3 +1,6 @@
+import numpy as np
+import random
+import sys
 class node:
     def __init__(self, parent, state, action, pathCost):
         self.parent=parent          #parent node
@@ -20,39 +23,69 @@ class N_Puzzle:
     def __init__(self, goalState, size):
         self.goal=goalState
         self.size=size #length of rectangle, 3 for 8 puzzle, 4 for 15 puzlle 5 for 24 puzzle
-         
 
     #What other functions will we need in the N_puzzle Class
+    def findTile(self,puzzle,tile):
+        for row in range(len(puzzle)):
+            for col in range(len(puzzle)):
+                if puzzle[row][col]==tile:
+                    return row,col
 
     def calculateHeuristicCost(self):
         print("Calculate Stuff")
 
     def generateRandomPuzzle(self):
-        print("Shake things up here")
+        solvable=False
+        values=[0,*range(1,self.size*self.size)]
+
+        while (not solvable):
+            random.shuffle(values)
+            puzzle= np.reshape(values,(self.size,self.size))
+            solvable=self.checkIfSolvable(puzzle)
+        self.data=puzzle
+
+    def checkIfSolvable(self,puzzle):
+        size=len(puzzle)
+        inversions = 0
+        for i in range(size*size-1):
+            for j in range(i+1,size*size):
+                tile1=puzzle[i//size][i%size]
+                tile2=puzzle[j//size][j%size]
+                #Make sure not to count the empty tile
+                if(tile1 != 0 and tile2 != 0 and tile1 > tile2):
+                    inversions+=1
+            
+        
+        if (size%2==1 ):
+            return inversions%2 == 0
+        else:
+            row,col=self.findTile(puzzle,0)
+            if (row%2==1 and inversions%2==1) or (row%2==0 and inversions%2==0):
+                return True
+            else:
+                return False
+        
 
     def solve(self):
         print("Do Stuff")
 
 
 def main():
-    goal8=[[0,1,2],
-          [3,4,5],
-          [6,7,8]]
-    goal15=[    [0 ,1 ,2,  3],
-                [4 ,5 ,6,  7],
-                [8 ,9 ,10,11],
-                [12,13,14,15]]
-    goals24=[[0 ,1 ,2, 3, 4],
-             [5, 6, 7, 8, 9 ],
-             [10,11,12,13,14],
-             [15,16,17,18,19],
-             [20,21,22,23,24]]
+    size=int(sys.argv[1]) # 3, 4, 5 for 8, 15 and 24 puzzle respectively
+    goal = np.reshape([*range(size*size)],(size,size))
 
-
-    puzzle = N_Puzzle(goal8,8)
-    puzzle.generateRandomPuzzle()
-
-    puzzle.solve()
+    puzzle = N_Puzzle(goal,size)
+    
+    for i in range(10):
+        puzzle.generateRandomPuzzle()
+        print("Puzzle #"+str(i+1))
+        print(puzzle.data)
+        puzzle.solve()
+        print("\n")
+        
+    print("The goal state is:")
+    print(goal)
+   
 
 
 if __name__ == "__main__":
