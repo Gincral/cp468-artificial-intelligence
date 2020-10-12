416591dc9a0006360903eeb2f7a1d391a7e1230e
benkeun <bknarfk@gmail.com>
2020-10-11 21:05:03 -0400

    Improved Space Complexity and added linear conflict heuristic from Nina

--- asgn01/8-puzzle.py
+++ asgn01/8-puzzle.py
@@ -2,15 +2,14 @@ import numpy as np
 import random
 from queue import PriorityQueue
 import sys
+import csv
 class node:
-    # TODO: Are we allowed to store the heuristic cost on the node?
-    def __init__(self, parent, state, action, pathCost, heuristicCost):
-        self.parent=parent          #parent node
+    def __init__(self, state, action, pathCost, heuristicCost):
         self.state=state            #state/data, position of all tiles
         self.action=action          #action required to get to this state from parent
         self.pathCost=pathCost      #cost from initial state to this node
         self.heuristicCost = heuristicCost #informed heuristic cost
-        self.children= []           #list of Children
+
 
     
     def __eq__(self, other):
@@ -21,8 +20,6 @@ class node:
 
 
 class N_Puzzle:
-    nodesExplored=0 #Do we even need to store this like this?
-
     def __init__(self, goalState, size):
         self.goal=goalState
         self.size=size #length of rectangle, 3 for 8 puzzle, 4 for 15 puzlle 5 for 24 puzzle
@@ -55,8 +52,36 @@ class N_Puzzle:
                         h2 += manhattan
             return h2
         elif self.heuristic == "h3":
-            h3=100
+            # Linear Conflict + Manhattan Distance/Taxicab geometry 
+            h3 = 0
+            conflictCount = 0
+            for row in range(len(puzzle)):
+                for col in range(len(puzzle)):
+                    if puzzle[row][col] != 0:
+                        tile = puzzle[row][col]
+                        goalPos = self.findTile(self.goal, tile)
+                        manhattan = abs(row - goalPos[0]) + abs(col - goalPos[1])
+                        h3 += manhattan
+                        conflictCount = self.linearConflict([row, col], tile, puzzle, self.goal)
+                        h3 += conflictCount*2
             return h3
+    
+    def linearConflict(self, tileLocation, tile, puzzel, goal):
+        conflictCount = 0
+        tileGoal = self.findTile(goal, tile)
+        if (tileLocation[0]==tileGoal[0] and (tileGoal[1]-tileLocation[1])>0):
+            for i in range(tileLocation[1]+1, len(goal)):
+                target = puzzel[tileLocation[0]][i]
+                if target!=0:
+                    targetGoal = self.findTile(goal, target)
+                    if (targetGoal[0]==tileGoal[0] and targetGoal[1]<tileGoal[1]): conflictCount+=1
+        elif (tileLocation[1]==tileGoal[1] and (tileGoal[0]-tileLocation[0])>0):
+            for i in range(tileLocation[0]+1, len(goal)):
+                target = puzzel[i][tileLocation[1]]
+                if target!=0:
+                    targetGoal = self.findTile(goal, target)
+                    if (targetGoal[1]==tileLocation[1] and targetGoal[0]<tileGoal[0]): conflictCount+=1
+        return conflictCount
 
 
     def generateRandomPuzzle(self):
@@ -98,15 +123,15 @@ class N_Puzzle:
         children = []
         # Try to create up to 4 new possible states by moving a tile into the empty space
         # Move tile up
-        print('parent state:')
-        print(parentNode.state)
+        #print('parent state:')
+        #print(parentNode.state)
         if int(row) > 0 and parentNode.action!="DOWN":
             newState = np.reshape(parentNode.state.copy(), (self.size,self.size))
             newState[row][col] = parentNode.state[row-1][col]
             newState[row-1][col] = 0
             #print('move tile up')
             #print(newState)
-            children.append(node(parentNode, newState, "UP", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
+            children.append(node( newState, "UP", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
         # Move tile down
         if int(row) < self.size - 1 and parentNode.action!="UP":  
             newState = np.reshape(parentNode.state.copy(), (self.size,self.size))
@@ -114,30 +139,30 @@ class N_Puzzle:
             newState[row+1][col] = 0
             #print('move tile down')
             #print(newState)
-            children.append(node(parentNode, newState, "DOWN", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
+            children.append(node( newState, "DOWN", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
         # Move tile right
         if int(col) > 0 and parentNode.action!="RIGHT":
             newState = np.reshape(parentNode.state.copy(), (self.size,self.size))  
             newState[row][col] = parentNode.state[row][col-1]
             newState[row][col-1] = 0
-            print('move tile left')
-            print(newState)
-            children.append(node(parentNode, newState, "LEFT", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
+            #print('move tile left')
+            #print(newState)
+            children.append(node( newState, "LEFT", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
         # Move tile left
         if int(col) < self.size - 1 and parentNode.action!="LEFT":
             newState = np.reshape(parentNode.state.copy(), (self.size,self.size))  
             newState[row][col] = parentNode.state[row][col+1]
             newState[row][col+1] = 0
-            print('move tile right')
-            print(newState)
-            children.append(node(parentNode, newState, "RIGHT", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
+            #print('move tile right')
+            #print(newState)
+            children.append(node( newState, "RIGHT", parentNode.pathCost + 1, self.calculateHeuristicCost(newState)))
         return children
 
     def setHeuristic(self, heuristic):
         self.heuristic = heuristic
 
     def solve(self):
-        root = (self.calculateHeuristicCost(self.data),0,node(None, self.data, None, 0, self.calculateHeuristicCost(self.data)))
+        root = (self.calculateHeuristicCost(self.data),0,node(self.data, None, 0, self.calculateHeuristicCost(self.data)))
         frontier = PriorityQueue()
         frontier.put(root)
         nodesExpanded = 0
@@ -147,40 +172,40 @@ class N_Puzzle:
         # temp variable just so it doesn't infinitely loop
         i = 1
         newNode = None
-        while not (frontier.empty()):
+        while (not frontier.empty()) and nodesExpanded<15000000:
 
             newNode = frontier.get()[2]
-            print('popped node:')
-            if nodesExpanded%10000==0:
-                print("\n")
+            #print('popped node:')
+            if nodesExpanded%50000==0:
+                #print("\n")
                 print(nodesExpanded)
                 print(newNode.state)
                 print(newNode.heuristicCost)
                 print(newNode.pathCost)
-            if (np.array_equal(newNode.state, self.goal)):
-                print('goal reached!')
-                print('result:')
-                print(newNode.state)
-                return nodesExpanded, newNode.pathCost
-            else:
-                childNodes = self.expandNode(newNode)
-                nodesExpanded += 1
-                # TODO: check if state has been seen and check cost before adding to frontier
-                for child in childNodes:
-                    key = str(child.state)
-                    print(key not in reached)
-                    print('Child cost: ' + str(child.pathCost + child.heuristicCost))
-                    if key in reached:
-                        reachedCost = reached[key].pathCost + reached[key].heuristicCost
-                        print('Reached state cost: ' + str(reachedCost))
-                    if key not in reached or (child.pathCost + child.heuristicCost) < (reachedCost):
-                        print('adding child:')
-                        print(child.state)
-                        reached[key] = child
-                        frontier.put((child.heuristicCost+child.pathCost,i,child))
-                        i+=1
+ 
+            childNodes = self.expandNode(newNode)
+            nodesExpanded += 1
+            # TODO: check if state has been seen and check cost before adding to frontier
+            for child in childNodes:
+                key = str(child.state)
+                #print(key not in reached)
+                #print('Child cost: ' + str(child.pathCost + child.heuristicCost))
+                if key in reached:
+                    reachedCost = reached[key].pathCost + reached[key].heuristicCost
+                    #print('Reached state cost: ' + str(reachedCost))
+                if key not in reached or (child.pathCost + child.heuristicCost) < (reachedCost):
+                    #print('adding child:')
+                    #print(child.state)
+                    reached[key] = child
+                    frontier.put((child.heuristicCost+child.pathCost+child.heuristicCost/1000.0,i,child))
+                    i+=1
+                if (np.array_equal(child.state, self.goal)):
+                    #print('goal reached!')
+                    #print('result:')
+                    #print(newNode.state)
+                    return nodesExpanded, newNode.pathCost
                
-        raise Exception("Failed to solve puzzle with start state:" + self.data)
+        return (nodesExpanded,-1)
 
 def main():
     if len(sys.argv) == 1 or sys.argv[1] == '0':
@@ -190,23 +215,32 @@ def main():
     size=int(sys.argv[1]) # 3, 4, 5 for 8, 15 and 24 puzzle respectively
     goal = np.reshape([*range(size*size)],(size,size))
 
-    
-    nodesExpanded=[]
-    moves=[]
-    for i in range(5):
+    #puzzle = N_Puzzle(goal,size)
+    #puzzle.setHeuristic("h3")
+    #puzzle.data=[[0,1,2],
+    #            [5,4,3],
+    #            [6,7,8]]
+    #print(puzzle.calculateHeuristicCost(puzzle.data))
+    results=[["H1 Nodes Expanded"],["H1 Moves"], ["H2 Nodes Expanded"], ["H2 Moves"]]
+    for i in range(1):
         puzzle = N_Puzzle(goal,size)
         puzzle.generateRandomPuzzle()
         print("Puzzle #"+str(i+1))
         print(puzzle.data)
-        puzzle.setHeuristic("h2")
-        results=puzzle.solve()
-        nodesExpanded.append(results[0])
-        moves.append(results[1])
+        resulth1=[0,0]
+        #puzzle.setHeuristic("h1")
+        #resulth1=puzzle.solve()
+        #puzzle.setHeuristic("h2")
+        #resulth2=puzzle.solve()
+        puzzle.setHeuristic("h3")
+        resulth3=puzzle.solve()
+        results.append([resulth1[0],resulth1[1],resulth2[0],resulth2[1],resulth3[0],resulth3[1]])
         print("\n")
-        
-    for i in range(len(moves)):
-        print("Nodes: "+str(nodesExpanded[i])+"  Moves: " +str(moves[i]))
-
-
+    
+    myFile = open('15-puzzleWithH3.csv', 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(results)
+    
 if __name__ == "__main__":
     main()
