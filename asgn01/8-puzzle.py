d19b9660dba20c62130c056c10515c7499664175
benkeun <bknarfk@gmail.com>
2020-10-18 14:07:18 -0400

    Simplified and added documentation for final submission

--- asgn01/8-puzzle.py
+++ asgn01/8-puzzle.py
@@ -10,75 +10,68 @@ class node:
         self.pathCost=pathCost      #cost from initial state to this node
         self.heuristicCost = heuristicCost #informed heuristic cost
 
-    def __eq__(self, other):
-        if self.__class__ != other.__class__:
-            return False
-        else:
-            return self.state == other.state
-
 class N_Puzzle:
+    absolute=abs #locally saving absolute function to increase speed of heuristic calculations
     def __init__(self, goalState, size):
         self.goal=goalState
         self.goalIndex={}
-        self.numRows=int(np.sqrt(size))
         for i in range(size):
             self.goalIndex[goalState[i]]=i
+        self.size=size #length of array, 9 for 8 puzzle, 16 for 15 puzzle 25, for 24 puzzle
+        self.numRows=int(np.sqrt(size)) #number of rows/columns
 
-        self.size=size #length of array, 3 for 8 puzzle, 4 for 15 puzlle 5 for 24 puzzle
-
-    #What other functions will we need in the N_puzzle Class
     def findTile(self,puzzle,tile):
-        for row in range(self.size):
-                if puzzle[row]==tile:
-                    return row
+        for index in range(self.size):
+                if puzzle[index]==tile:
+                    return index
 
     def calculateHeuristicCost(self, puzzle):
         if self.heuristic == "h1":
             # Calculation for h1: Number of misplaced tiles
             h1 = 0
-            for space in range(self.size):
-                    if puzzle[space] != 0 and puzzle[space] != self.goal[space]:
+            for tile in range(self.size):
+                    if puzzle[tile] != 0 and puzzle[tile] != self.goal[tile]:
                         h1 += 1
             return h1
         elif self.heuristic == "h2":
             # Calculation for h2: Total Manhattan distance
             h2 = 0
             for row in range(self.size):
-                    if puzzle[row] != 0:
-                        tile = puzzle[row]
-                        goalPos = self.goalIndex[tile]
-                        manhattan = abs(row//self.numRows - goalPos//self.numRows) + abs(row%self.numRows - goalPos%self.numRows)
-                        h2 += manhattan
+                if puzzle[row] != 0:
+                    tile = puzzle[row]
+                    goalPos = self.goalIndex[tile]
+                    manhattan = self.absolute(row//self.numRows - goalPos//self.numRows) + self.absolute(row%self.numRows - goalPos%self.numRows)
+                    h2 += manhattan
             return h2
         elif self.heuristic == "h3":
             # Linear Conflict + Manhattan Distance/Taxicab geometry 
             h3 = 0
             conflictCount = 0
-            for row in range(self.size):
-                    if puzzle[row] != 0:
-                        tile = puzzle[row]
-                        goalPos = self.goalIndex[tile]
-                        manhattan = abs(row//self.numRows - goalPos//self.numRows) + abs(row%self.numRows - goalPos%self.numRows)
-                        h3 += manhattan
-                        conflictCount = self.linearConflict(row, tile, puzzle,self.goal)
-                        h3 += conflictCount*2
+            for index in range(self.size):
+                if puzzle[index] != 0:
+                    tile = puzzle[index]
+                    goalPos = self.goalIndex[tile]
+                    manhattan = self.absolute(index//self.numRows - goalPos//self.numRows) + self.absolute(index%self.numRows - goalPos%self.numRows)
+                    h3 += manhattan
+                    conflictCount = self.linearConflict(index, tile, puzzle,self.goal)
+                    h3 += conflictCount*2 #every conflict requires at least 2 moves to fix
             return h3
-    
-    def linearConflict(self, tileLocation, tile, puzzle, goal):
+    #count how many times two tiles are in the same row but must pass each other to reach their goal positions
+    def linearConflict(self, index, tile, puzzle, goal):
         conflictCount = 0
         tileGoal = self.goalIndex[tile]
-        if (tileLocation//self.numRows==tileGoal//self.numRows and (tileGoal%self.numRows-tileLocation%self.numRows)>0): #right row
-            for i in range((tileLocation%self.numRows)+1, self.numRows):
-                target = puzzle[self.numRows*(tileLocation//self.numRows)+i]
+        if (index//self.numRows==tileGoal//self.numRows and (tileGoal%self.numRows-index%self.numRows)>0): #right row
+            for i in range((index%self.numRows)+1, self.numRows):
+                target = puzzle[self.numRows*(index//self.numRows)+i]
                 if target!=0:
                     targetGoal = self.goalIndex[target]
                     if (targetGoal//self.numRows==tileGoal//self.numRows and targetGoal%self.numRows<tileGoal%self.numRows): conflictCount+=1
-        elif (tileLocation//self.numRows==tileGoal//self.numRows and (tileGoal%self.numRows-tileLocation%self.numRows)>0):
-            for i in range(tileLocation//self.numRows+1, self.numRows):
-                target = puzzle[i%self.numRows+self.numRows*(tileLocation//self.numRows)]
+        if (index//self.numRows==tileGoal//self.numRows and (tileGoal%self.numRows-index%self.numRows)>0):
+            for i in range(index//self.numRows+1, self.numRows):
+                target = puzzle[i%self.numRows+self.numRows*(index//self.numRows)]
                 if target!=0:
                     targetGoal = self.goalIndex[target]
-                    if (targetGoal%self.numRows==tileLocation%self.numRows and targetGoal//self.numRows<tileGoal//self.numRows): conflictCount+=1
+                    if (targetGoal%self.numRows==index%self.numRows and targetGoal//self.numRows<tileGoal//self.numRows): conflictCount+=1
         return conflictCount
     
     def calcInversions(self,puzzle):
@@ -93,16 +86,12 @@ class N_Puzzle:
                     inversions+=1
         return inversions
 
-
+    #generate random Puzzle and check that it is solvable before setting the data
     def generateRandomPuzzle(self):
         solvable=False
-        values=[0,*range(1,self.size)]
-
+        puzzle=[0,*range(1,self.size)]
         while (not solvable):
-            random.shuffle(values)
-            puzzle= values
-
-
+            random.shuffle(puzzle)
             solvable=self.checkIfSolvable(puzzle)
         self.data=puzzle
 
@@ -122,12 +111,11 @@ class N_Puzzle:
         self.heuristic = heuristic
 
     def expandNode(self, parentNode):
-        #print('expanding node')
         emptyTilePos = self.findTile(parentNode.state,0)
         row = int(emptyTilePos//self.numRows)
         col = int(emptyTilePos%self.numRows)
         children = []
-        # Try to create up to 4 new possible states by moving a tile into the empty space
+        # Try to create up to 3 new possible states by moving a tile into the empty space, avoiding reversing previous move
         # Move tile up
         if int(row) > 0 and parentNode.action!="DOWN":
             newState = parentNode.state.copy()
@@ -157,41 +145,29 @@ class N_Puzzle:
     def solve(self):
         root = (self.calculateHeuristicCost(self.data),0,node(self.data, None, 0, self.calculateHeuristicCost(self.data)))
         frontier = []
-        heapq.heappush(frontier,root)
-        reached = {}
-        print('solving...')
-        i=0
-        # temp variable just so it doesn't infinitely loop
+        heapq.heappush(frontier,root) #adds root to frontier using heap method which always keeps smallest node in index 0
+        reached = {} #stores the nodes that have already been searched
+        i=0 # ensures every node has unique priority without ordering the states outside of heuristic and path cost
         nodesExpanded = 0
         newNode = None
-        while (frontier) and nodesExpanded<15000000:
+        while (frontier) and nodesExpanded<15000000: #continue until frontier is empty or nodes expanded reaches 15 million
 
             newNode = heapq.heappop(frontier)[2] # Retrives first Node in priority Queue
-            if nodesExpanded%50000==0:
-                #print("\n")
-                print(nodesExpanded)
-
-                print(newNode.state)
-                print(newNode.heuristicCost)
-                print(newNode.pathCost)
- 
-            childNodes = self.expandNode(newNode)
+            if (newNode.state==self.goal):
+                return nodesExpanded, newNode.pathCost
+            childNodes = self.expandNode(newNode) #find children node
             nodesExpanded += 1
             for child in childNodes:
                 key = str(child.state)
-                #print(key)
                 if key in reached:
                     reachedCost = reached[key].pathCost + reached[key].heuristicCost
-                    #print('Reached state cost: ' + str(reachedCost))
                 if key not in reached or reachedCost>child.heuristicCost+child.pathCost:
                     reached[key] = child
-                    childTuple=(child.heuristicCost+child.pathCost+child.heuristicCost/100.0,i,child)
-                    #print(childTuple)
-                    heapq.heappush(frontier,childTuple)
+                    childTuple=(child.heuristicCost+child.pathCost,i,child)
+                    heapq.heappush(frontier,childTuple) #pushes child into heap queue
                     i+=1
-                if (child.state==self.goal):
-                    return nodesExpanded, newNode.pathCost+1
-               
+
+        #no solution found in 15million states
         return (nodesExpanded,-1)
 
 def main():
@@ -202,15 +178,14 @@ def main():
     size=int(sys.argv[1]) # 3, 4, 5 for 8, 15 and 24 puzzle respectively
     goal = [*range(size*size)]
 
-    myFile = open('8WithHeapRow.csv', 'w')
+    myFile = open('PreSubTest.csv', 'w', newline='\n') #File that the nodes expanded and moves are saved too for each puzzle
     writer = csv.writer(myFile)
-    
+    writer.writerows([["H1 Nodes","H1 Moves","H2 Nodes","H2 Moves","H3 Nodes","H3 Moves",]])
     for i in range(100):
         puzzle = N_Puzzle(goal,size*size)
         puzzle.generateRandomPuzzle()
         print("Puzzle #"+str(i+1))
         print(puzzle.data)
-        resulth1=[0,0]
         puzzle.setHeuristic("h1")
         resulth1=puzzle.solve()
         puzzle.setHeuristic("h2")
@@ -218,9 +193,6 @@ def main():
         puzzle.setHeuristic("h3")
         resulth3=puzzle.solve()
         results=[[resulth1[0],resulth1[1],resulth2[0],resulth2[1],resulth3[0],resulth3[1]]]
-    
-
         writer.writerows(results)
-    
 if __name__ == "__main__":
     main()
