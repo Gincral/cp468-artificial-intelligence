e459335831b24de0c0776c32be287f44003608ed
benkeun <bknarfk@gmail.com>
2020-11-03 17:54:59 -0500

    Fixed square variables and added related cells list

--- asgn02/ac3-sudoku.py
+++ asgn02/ac3-sudoku.py
@@ -1,7 +1,8 @@
 rows =['A','B','C','D','E','F','G','H','I']
 class AC3:
 
-    def __init__(self, data):
+    def __init__(self, data,constraints):
+        self.binConstraints=constraints
         self.values={}
         cols= [x for x in range(1,10)]
         self.variables = [row+str(col) for row in rows for col in cols]
@@ -12,7 +13,22 @@ class AC3:
             else:
                 self.values[var]=[int(data[i])]
             i += 1
+        self.generate_related_cells()
 
+    def generate_related_cells(self):
+        related_cells = {}
+
+        #for each one of the 81 cells
+        for cell in self.variables:
+
+            related_cells[cell] = []
+
+            # related cells are the ones that current cell has constraints with
+            for constraint in self.binConstraints:
+                if cell == constraint[0] and constraint[1] not in related_cells[cell]:
+                    related_cells[cell].append(constraint[1])
+
+        self.related_cells=related_cells
 
 def CreateConstraints():
     
@@ -33,14 +49,19 @@ def CreateConstraints():
                     constraintPairs.append([var1,var2])
     for i in range(3):
         for j in range(3):
-            for k in range(3):
-                squareVariables= [rows[3*i]+str(3*j+k), rows[3*i+1]+str(3*j+k),rows[3*i+2]+str(3*j+k)]
+            squareVariables=[]
+            for k in range(1,4):
+                squareVariables.append(rows[3*i]+str(3*j+k))
+                squareVariables.append(rows[3*i+1]+str(3*j+k))
+                squareVariables.append(rows[3*i+2]+str(3*j+k))
+            print(squareVariables)
             for var1 in squareVariables:
                 for var2 in squareVariables:
                     if var1!=var2 and [var1,var2] not in constraintPairs:
                         constraintPairs.append([var1,var2])
 
     print("There are 81 Variables.\nThere are "+ str(len(constraintPairs))+ " Arcs")
+    return constraintPairs
 
 
 
@@ -52,10 +73,11 @@ def main():
     #     return 0
 
     data = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
-    CreateConstraints()
-    x = AC3(data)
-    print(x.variables)
-    print(x.values)
+    
+    x = AC3(data,CreateConstraints())
+    
+    #print(x.variables)
+    #print(x.values)
 
 if __name__ == "__main__":
     main()
