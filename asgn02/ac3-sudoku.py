2de53284670b6857c696c9568d29b2b232cd4017
MorgenneB <morgenne.b@gmail.com>
2020-11-06 14:32:31 -0500

    Sudoku backtracking

--- asgn02/ac3-sudoku.py
+++ asgn02/ac3-sudoku.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 rows =['A','B','C','D','E','F','G','H','I']
 class AC3:
 
@@ -7,12 +9,15 @@ class AC3:
         cols= [x for x in range(1,10)]
         self.variables = [row+str(col) for row in rows for col in cols]
         i = 0
-        for var in self.variables:
-            if data[i] == '0':
-                self.values[var]=[1,2,3,4,5,6,7,8,9]
-            else:
-                self.values[var]=[int(data[i])]
-            i += 1
+        if isinstance(data, str):
+            for var in self.variables:
+                if data[i] == '0':
+                    self.values[var]=[1,2,3,4,5,6,7,8,9]
+                else:
+                    self.values[var]=[int(data[i])]
+                i += 1
+        else:
+            self.values = data
         self.generate_related_cells()
 
     def generate_related_cells(self):
@@ -57,10 +62,15 @@ class AC3:
     def printInFormat(self):
         for row in rows:
             print(self.values[row+'1'],self.values[row+'2'],self.values[row+'3'],self.values[row+'4'],self.values[row+'5'],self.values[row+'6'],self.values[row+'7'],self.values[row+'8'],self.values[row+'9'],'\n')
-        
+
+    def find_unsolved_square(self):
+        for item in self.values.items():
+            if len(item[1]) > 1:
+                return item[0]
+        return None        
 
 def CreateConstraints():
-    
+
     constraintPairs=[]
 
     for row in rows:
@@ -93,9 +103,30 @@ def CreateConstraints():
     return constraintPairs
 
 
+class BacktrackSearch:
+    def __init__(self, partial_assignment):
+        self.partial_assignment=partial_assignment
 
-
-
+    def backtrack_search(self):
+        return self.backtrack(self.partial_assignment)
+    
+    def backtrack(self, possible_solution):
+        unsolved = possible_solution.find_unsolved_square()
+        if unsolved is None:
+            return possible_solution
+        # TODO: create a getter for values
+        unsolved_domain = possible_solution.values[unsolved]
+        # For each possible assignment, try to solve the puzzle
+        for value in unsolved_domain:
+            solution = AC3(deepcopy(possible_solution.values), CreateConstraints())
+            solution.values[unsolved] = [value]
+            # If the value did not result in an inconsistency, go to the next unassigned square
+            if solution.ac3():
+                result = self.backtrack(solution)
+                if result is not False:
+                    return result
+        
+        return False
 
 def main():
     # data = input("enter 81 length string: ")
@@ -103,11 +134,30 @@ def main():
     #     print("wrong input")
     #     return 0
 
+    # data = "026000378058637400047000561000720900000308250802000010469501000001900740030040090"
+
     data = "000020600900005001001806400008102900700000008006708200002609500800200009005010000"
     
+    # data = "020000003600031000500000084370000501000060009000400000000007800200090040050200100"
+    
+    # The "world's hardest Sudoku"
+    # https://puzzling.stackexchange.com/questions/252/how-do-i-solve-the-worlds-hardest-sudoku
+    # data = "800000000003600000070090200050007000000045700000100030001000068008500010090000400"
+
+    # data = "0"*81
+
     x = AC3(data,CreateConstraints())
-    print(x.ac3())
+    x.ac3()
     # print(x.related_cells)
     x.printInFormat()
+    if (x.find_unsolved_square() is None):
+        print('solved')
+        x.printInFormat()  
+    else:
+        print("backtracking")
+        search = BacktrackSearch(x)
+        solution = search.backtrack_search()
+        solution.printInFormat()
+
 if __name__ == "__main__":
     main()
