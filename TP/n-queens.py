ee8afb635ba90e2dea0cd3fa5fab8cb2dafdd69b
benkeun <bknarfk@gmail.com>
2020-12-01 14:45:18 -0500

    sped up conflict calculations

--- TP/n-queens.py
+++ TP/n-queens.py
@@ -10,6 +10,9 @@ class NQueens:
     def __init__(self, n):
         self.size = n
         self.puzzle = self.generatePuzzle()
+        self.queensRow=[0]*n
+        self.queensDiag1=[0]*(2*n)
+        self.queensDiag2=[0]*(2*n)
 
     def generatePuzzle(self):
         nr = self.size
@@ -26,31 +29,43 @@ class NQueens:
                 board[i+1]=i*2
             for i in range(nr//2,nr):
                 board[i]=random.randint(0,nr-1)
-        return board;
+        return board
 
     def printPuzzle(self):
         print(self.puzzle)
         size = self.size
-        for i in range(size):
+        """for i in range(size):
             row = ['[ ]'] * size
             for col in range(size):
                 if self.puzzle[col] == i:
                     row[col] = '[Q]'
             print(''.join(row))
+            """
 
-    def conflicts(self, col, value):
+    def conflicts(self, col, row):
         total = 0
-        for i in range(self.size):
-            if i == col:
-                continue
-            if self.puzzle[i] == value or abs(i - col) == abs(self.puzzle[i] - value):
-                total += 1
+        total+=self.queensRow[row]
+        total+=self.queensDiag1[row+col]
+        total+=self.queensDiag2[self.size-row+col]-3
         return total
 
-    def minConflicts(self, maxSteps=1000):
+    def populateFrequencies(self):
+        for i in range(self.size):
+            row=self.puzzle[i]
+            self.queensRow[row]+=1
+            self.queensDiag1[row+i]+=1
+            self.queensDiag2[self.size-row+i]+=1
+
+
+
+    def minConflicts(self, maxSteps=100000):
+        self.populateFrequencies()
         for i in range(maxSteps):
             conflicts = [self.conflicts(col, self.puzzle[col]) for col in range(self.size)] 
-            if sum(conflicts) == 0:
+            sumCon=sum(conflicts)
+            if i%10==0:
+                print(sumCon)
+            if sumCon == 0:
                 return True
             conflicting=[]
             for i in range(len(conflicts)):
@@ -59,17 +74,25 @@ class NQueens:
             position = conflicting[random.randrange(0, len(conflicting))]
             list = [self.conflicts(position, value) for value in range(self.size)]
             n = random.choice([i for i in range(self.size) if list[i] == min(list)])
+            prev=self.puzzle[position]
             self.puzzle[position] = n
+            self.queensRow[n]+=1
+            self.queensRow[prev]-=1
+            self.queensDiag1[n+position]+=1
+            self.queensDiag1[prev+position]-=1
+            self.queensDiag2[self.size-n+position]+=1
+            self.queensDiag2[self.size-prev+position]-=1
         return False
     
 
 
 def main():
     # n = int(input("Enter the value of N: "))
-    nqueens = NQueens(100)
-    nqueens.printPuzzle()
+    nqueens = NQueens(1000)
+    #nqueens.printPuzzle()
     if nqueens.minConflicts():
-        nqueens.printPuzzle()
+        print("solved")
+        #nqueens.printPuzzle()
     else:
         print("Puzzle cant be solved, try upper the iteraition")
 
